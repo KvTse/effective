3b6b63a4d93bf54559173db63274e7df1f6d2c2b
xietongqing <xietongqing@teddymobile.cn>
2019-07-23 00:25:49 +0800

    提交策略注释信息

--- src/main/java/cn/kvtse/study/designer/behavior/strategy/Client.java
+++ src/main/java/cn/kvtse/study/designer/behavior/strategy/Client.java
@@ -2,7 +2,7 @@ package cn.kvtse.study.designer.behavior.strategy;
 
 /**
  * 客户端
- *
+ * 策略模式是以合成聚合来替代继承的关系
  * @author Tsing Tse
  * @date 2019-07-23 0:06
  */
@@ -10,6 +10,7 @@ public class Client {
     public static void main(String[] args) {
         WildDuck wildDuck = new WildDuck();
         wildDuck.setIFlyBehavior(new GoodFlyBehavior());
+        wildDuck.setIFlyBehavior(new BadFlyBehavior());
         wildDuck.fly();
     }
 }
